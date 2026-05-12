b731fefa3a5d2e8688af24b9634a2753621df1dd
Khirul Anuar <mdkhirulanuar@gmail.com>
2026-05-13 06:31:40 +0800

    Update service worker cache version and fetch logic
    Updated service worker to use new cache version and improved fetch handling to cache responses from 'tesseract'.

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE = 'metercalc-v2';
+const CACHE = 'metercalc-v4';
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(CACHE).then(c => c.addAll([
             '/meter-calc-universal/',
             '/meter-calc-universal/index.html',
             '/meter-calc-universal/css/style.css',
+            '/meter-calc-universal/js/snap.js',
+            '/meter-calc-universal/js/ocr.js',
             '/meter-calc-universal/js/calculator.js',
             '/meter-calc-universal/js/ui.js',
             '/meter-calc-universal/js/app.js'
@@ -12,5 +14,13 @@ self.addEventListener('install', e => {
     );
 });
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+            if (res.ok && res.url.includes('tesseract')) {
+                const clone = res.clone();
+                caches.open(CACHE).then(c => c.put(e.request, clone));
+            }
+            return res;
+        }))
+    );
 });
