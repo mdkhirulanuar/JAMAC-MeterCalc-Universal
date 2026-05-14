fae53118453bc07ea46a19e363ae52cee8a54f59
Khirul Anuar <mdkhirulanuar@gmail.com>
2026-05-14 21:20:04 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'metercalc-v3.0.1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/calculator.js',
+    './js/ui.js',
+    './js/app.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+        )).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            if (cached) return cached;
+            return fetch(event.request).then(response => {
+                const clone = response.clone();
+                caches.open(CACHE).then(cache => cache.put(event.request, clone));
+                return response;
+            }).catch(() => caches.match('./index.html'));
+        })
+    );
+});
